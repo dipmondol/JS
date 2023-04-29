7118ca3d4ba3c00ce940ee3e5dfc873adac2b1de
dipmondol <mondoldip76@gmail.com>
2023-04-29 22:56:31 +0600

    html quiz app backend developed

---
+++ quiz-app/js/main.js
@@ -0,0 +1,82 @@
+const nextBtn = document.querySelector(".nextBtn");
+let que_count = 0;
+
+showQuestions(0);
+
+function showQuestions(index) {
+  const que_text = document.querySelector(".text");
+  const option_list = document.querySelector(".quiz-options");
+  const total_que = document.querySelector(".total-que");
+
+  let option_tag =
+    '<div class="options">' +
+    questions[index].options[0] +
+    "</div>" +
+    ' <div class="options">' +
+    questions[index].options[1] +
+    "</div>" +
+    '<div class="options">' +
+    questions[index].options[2] +
+    "</div>" +
+    '<div class="options">' +
+    questions[index].options[3] +
+    "</div>";
+
+  let que_tag =
+    "<span>" +
+    questions[index].num +
+    "." +
+    questions[index].question +
+    "</span>";
+
+  let total_queTag = "<p>" + questions[index].num + " of 5 Questions</p>";
+
+  que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+  total_que.innerHTML = total_queTag;
+
+  const option = option_list.querySelectorAll(".options");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+let tickIcon =
+  '<div class="right-icon"><ion-icon name="checkmark-outline"></ion-icon></div>';
+let crossIcon =
+  '<div class="close-icon"><ion-icon name="close-outline"></ion-icon></div>';
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  const option_list = document.querySelector(".quiz-options");
+  let allOptions = option_list.children.length;
+
+  if (userAns === correctAns) {
+    answer.classList.add("correct");
+    console.log("Answer Is correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
+  } else {
+    answer.classList.add("incorrect");
+    console.log("ans is wrong");
+    answer.insertAdjacentHTML("beforeend", crossIcon);
+
+    for (let i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correctAns) {
+        option_list.children[i].setAttribute("class", "options  correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+      }
+    }
+  }
+
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+}
+
+nextBtn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    showQuestions(que_count);
+  } else {
+    console.log("You have completed your task");
+  }
+};
---
+++ quiz-app/js/questions.js
@@ -0,0 +1,42 @@
+let questions = [
+  {
+    num: 1,
+    question: "What Does HTML Stand For?",
+    answer: "Hyper Text Markup Language",
+
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    num: 2,
+    question: "How many sizes of headers are available in HTML by default?",
+    answer: "6",
+
+    options: ["5", "1", "3", "6"],
+  },
+  {
+    num: 3,
+    question: "What is the smallest header in HTML by default?",
+    answer: "h1",
+
+    options: ["h2", "h3", "h1", "h6"],
+  },
+  {
+    num: 4,
+    question: "Choose the correct HTML element to define important text",
+    answer: ".html",
+
+    options: [".html", ".h", ".ht", "None of the above"],
+  },
+  {
+    num: 5,
+    question: "We enclose HTML tags within?",
+    answer: "<>",
+
+    options: ["{}", "<>", "!!", "None of the above"],
+  },
+];
