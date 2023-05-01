5ea1c285829e07105221ee7b0e383f6136e94721
dipmondol <mondoldip76@gmail.com>
2023-05-02 00:00:26 +0600

    progressbar & timelapse devloped

--- quiz-app/js/main.js
+++ quiz-app/js/main.js
@@ -1,7 +1,14 @@
 const nextBtn = document.querySelector(".nextBtn");
+const timeLine = document.querySelector(".time-lines");
 let que_count = 0;
+let counter;
+let timeValue = 15;
+let counterLine;
+let widthValue = 0;
 
 showQuestions(0);
+startTimer(timeValue);
+startTimeLine(0);
 
 function showQuestions(index) {
   const que_text = document.querySelector(".text");
@@ -44,7 +51,10 @@ let tickIcon =
   '<div class="right-icon"><ion-icon name="checkmark-outline"></ion-icon></div>';
 let crossIcon =
   '<div class="close-icon"><ion-icon name="close-outline"></ion-icon></div>';
+
 function optionSelected(answer) {
+  clearInterval(counter);
+  clearInterval(counterLine);
   let userAns = answer.textContent;
   let correctAns = questions[que_count].answer;
   const option_list = document.querySelector(".quiz-options");
@@ -69,14 +79,51 @@ function optionSelected(answer) {
 
   for (let i = 0; i < allOptions; i++) {
     option_list.children[i].classList.add("disabled");
+    // startTimer(15);
   }
+
+  nextBtn.style.display = "block";
 }
 
 nextBtn.onclick = () => {
   if (que_count < questions.length - 1) {
     que_count++;
     showQuestions(que_count);
+    clearInterval(counter);
+    startTimer(timeValue);
+
+    clearInterval(counterLine);
+    startTimeLine(widthValue);
+    nextBtn.style.display = "none";
   } else {
     console.log("You have completed your task");
   }
 };
+
+function startTimer(time) {
+  const timeCount = document.querySelector(".seconds");
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time + "s";
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = 0 + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "00";
+    }
+  }
+}
+
+function startTimeLine(time) {
+  counterLine = setInterval(timer, 50);
+  function timer() {
+    time += 2;
+    timeLine.style.width = time + "px";
+    if (time > 599) {
+      clearInterval(counterLine);
+    }
+  }
+}
